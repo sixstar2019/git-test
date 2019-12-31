d3b5de040216fefee457cbb774cc963ed77c7d4f
zhangsan <zhangsan@sixstar.cn>
2019-12-31 23:22:04 +0800

    修改FirstFClass

--- git-test-project/src/com/sixstar/git/FirstFClass.java
+++ git-test-project/src/com/sixstar/git/FirstFClass.java
@@ -8,10 +8,12 @@ public class FirstFClass {
         System.out.println("hello liuxing");
         System.out.println("hello liuxing");
         System.out.println("hello liuxing");
+        //hello test
     }
 
     public void test(){
         int i = 10;
         i += 10;
+        System.out.println(i);
     }
 }
