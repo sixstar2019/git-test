1c8b4d93c0a107fe5560e48668b74c73623b3d79
zhangsan <zhangsan@sixstar.cn>
2019-12-31 22:53:43 +0800

    把本地工厂添加到Git

---
+++ git-test-project/src/com/sixstar/git/FirstFClass.java
@@ -0,0 +1,12 @@
+package com.sixstar.git;
+
+public class FirstFClass {
+    public static void main(String[] args) {
+        System.out.println("hello liuxing");
+        System.out.println("hello liuxing");
+        System.out.println("hello liuxing");
+        System.out.println("hello liuxing");
+        System.out.println("hello liuxing");
+        System.out.println("hello liuxing");
+    }
+}
